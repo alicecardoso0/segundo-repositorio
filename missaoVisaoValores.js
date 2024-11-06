6be17ee86aa633737dbbf88c2932408be8731609
alicecardoso0 <163215190+alicecardoso0@users.noreply.github.com>
2024-11-05 21:22:58 -0300

    05/11 ll

---
+++ missaoVisaoValores.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Missão',
+    'Saiba qual é a missão de nossa empresa!',
+    'Nossa missão é realçar a sua beleza .'
+)
+
+criaCartao (
+    'Visão',
+    'Saiba qual é a visão de nossa empresa!',
+    'Nossa visão é cada vez mais crescer no mercado.'
+)
+
+criaCartao (
+    'Valores',
+    'Saiba quais são os valores de nossa empresa!',
+    'Nossos valores são entregar um trabalho de qualidade, com produtos de qualidade....'
+)
