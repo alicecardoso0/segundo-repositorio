24d1f6e20d96e1417e080b2d5254f1e4775ea1cf
alicecardoso0 <163215190+alicecardoso0@users.noreply.github.com>
2025-04-07 22:50:23 -0300

    07/04/2025

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener(‘DOMContentLoaded’, function(){
+    const aumentaFonteBotao = document.getElementById(‘aumentarfonte’);
+    aumentaFonteBotao.addEventListener(‘click’, function(){ 
+
+        })
+        const botaoDeAcessibilidade = Document.getElementById
+        ('botao-acessibilidade');
+        const opcoesDeAcessibilidade = Document.getElementById
+        ('opcoes-acessibilidade');
+   
+    })  
